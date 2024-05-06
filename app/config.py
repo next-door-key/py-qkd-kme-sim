dd9b3c1d67b636134bde3ec7d55337b9e33ba358
Ralfs Garkaklis <ralfs@garkaklis.com>
2024-05-06 19:18:48 +0300

    wip

--- app/config.py
+++ app/config.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     linked_to_kme: str
     linked_kme_id: str
 
-    rabbitmq_host: str = 'localhost'
-    rabbitmq_port: int = 5672
-    rabbitmq_shared_queue: str
+    mq_host: str = 'localhost'
+    mq_port: int = 5672
+    mq_username: str = 'guest'
+    mq_password: str = 'guest'
+    mq_shared_queue: str
