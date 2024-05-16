85a17834e1466249a1060fb322021c0f49c16e80
Ralfs Garkaklis <ralfs@garkaklis.com>
2024-05-16 13:02:24 +0300

    Configure exceptions to be per standard

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.dependencies import get_settings
 from app.internal.lifecycle import Lifecycle
@@ -25,6 +29,22 @@ app.add_middleware(HTTPSRedirectMiddleware)
 
 app.include_router(router=internal.router, prefix='/api/v1')
 app.include_router(router=keys.router, prefix='/api/v1')
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={'message': str(exc.detail)}
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({'message': 'Validation error', 'details': exc.errors()}),
+    )
 
 
 @app.get('/')
